779b74020e186ae8d4ff5bb6770cbc3e56ae4c79
Tolis Tsaganos <apostolos94@gmail.com>
2019-03-29 13:25:08 +0200

    running all tasks inside main.go

--- main.go
+++ main.go
@@ -1,8 +1,17 @@
 package main
 
-import "playground/pipeline"
+import (
+	"fmt"
+	"playground/faninfanout"
+	"playground/pipeline"
+)
 
 func main() {
+	fmt.Println("Runnning pipeline")
 	pipeline.Execute()
-}
+	fmt.Println("End of runnning pipeline")
 
+	fmt.Println("Runnning faninfanout")
+	faninfanout.Execute()
+	fmt.Println("End of runnning faninfanout")
+}
