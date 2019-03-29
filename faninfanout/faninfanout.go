59e5f640d30a7a68da43f86b9f427ce85f293234
Christos Petropoulos <chris.petropoulos91@gmail.com>
2019-03-29 13:19:16 +0200

    Move faninfanout example to appropriate place

---
+++ faninfanout/faninfanout.go
@@ -0,0 +1,88 @@
+package faninfanout
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+/*
+	The first stage, gen, is a function that converts a list of integers to a channel that emits the integers in the list.
+	The gen function starts a goroutine that sends the integers on the channel
+	and closes the channel when all the values have been sent
+*/
+
+func gen(numbers []int, delay time.Duration) chan int {
+	ch := make(chan int)
+	go func () {
+		for _, n := range numbers {
+			// time.Sleep(delay)
+			ch <- n // blocks go routine for every insertion since its unbuffered
+		}
+		close(ch)
+	}()
+
+	return ch;
+}
+
+/*
+	The second stage, sq, receives integers from a channel and returns a channel that emits the square of each received integer.
+	After the inbound channel is closed and this stage has sent all the values downstream, it closes the outbound channel
+ */
+
+func sq(numbers <-chan int) chan int {
+	square := make(chan int, 5)
+	go func () {
+
+		for n := range numbers {
+			square <-n*n
+			// time.Sleep(1000 * time.Millisecond)
+		}
+		close(square)
+	}()
+
+	return square;
+}
+
+func merge(channels ...<-chan int) <-chan int {
+	out := make(chan int)
+	var wg sync.WaitGroup
+
+	wg.Add(len(channels))
+
+	for _, c := range channels {
+		go func(ch <-chan int) {
+			for n := range ch {
+				out <- n
+			}
+			wg.Done()
+		}(c)
+	}
+
+	go func(wgc *sync.WaitGroup, c chan int) {
+		wg.Wait()
+		close(out)
+	}(&wg, out)
+
+	return out;
+}
+
+func Execute() {
+
+	numbers := []int{1, 2, 3, 4}
+
+	genNumbers := gen(numbers, 200 * time.Millisecond)
+
+
+	sqdWorker1 := sq(genNumbers)
+
+	sqdWorker2 := sq(genNumbers)
+
+	out := merge(sqdWorker1, sqdWorker2)
+
+	for n := range out {
+		fmt.Println(n)
+	}
+
+	// here trying to fetch data from the channel will return 0 as value and false as isOpen
+
+}
